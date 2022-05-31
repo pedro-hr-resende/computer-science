bfbba30dc2234f9bfdd9daed772a880cd8e3d31f
Pedro Ribeiro <80363868+pedro-hr-resende@users.noreply.github.com>
2022-05-31 13:19:45 -0300

    Delete algorithm-analysis/homework-001/c-code directory

--- algorithm-analysis/homework-001/c-code/generateData.h
+++
@@ -1,6 +0,0 @@
-/* Authors: Pedro Ribeiro, Gabriela Damazo & Isabelli Tedeschi
- * Description: Functions prototypes
- * Date: 28/05/2022 - Hour: 11:41
- */
-
-int* generateDataVector(int n, int range);
